82ad0b46661054136c09c70f650e9f23235190b0
Fabrizio Scarano <scaranofbr@gmail.com>
2021-07-06 19:43:38 +0200

    Implemented Biometric auth

--- lib/src/screen/login.dart
+++ lib/src/screen/login.dart
@@ -1,7 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:infopol_structure_test/src/model/login_response.dart';
 import 'package:infopol_structure_test/src/screen/menu_list.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+enum ButtonStatus { empty, full, loading }
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,12 +19,119 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  FlutterSecureStorage storage = FlutterSecureStorage();
+  LocalAuthentication auth = LocalAuthentication();
+  late bool _canCheckBiometrics;
+  late List<BiometricType> _availableBiomterics;
+  late bool _authenticated;
+
   late Future<http.Response> futureResponse;
+
+  TextEditingController _accountController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  late StreamController<String> _accountStream;
+  late StreamController<String> _passwordStream;
+  late StreamController<bool> _isLoadingStream;
+
+  @override
+  void initState() {
+    _accountStream = StreamController.broadcast();
+    _passwordStream = StreamController.broadcast();
+    _isLoadingStream = StreamController.broadcast();
+    _authenticateIfNeeded();   
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _accountController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _authenticateIfNeeded() async {
+    String? savedAccount = await storage.read(key: 'account');
+    String? savedPassword = await storage.read(key: 'password');
+    if (savedAccount != null && savedPassword != null) {
+      await _checkBiometrics();
+      if (!_canCheckBiometrics) return;
+      await _getAvailableBiometrics();
+      await _authenticate();
+      if (!_authenticated) return;
+      var response =
+          await _login(savedAccount, savedPassword);
+      var jsonStr = response.body;
+      if (response.statusCode != 200) return;
+      var loginResult = LoginResponse.fromJson(jsonStr);
+      if (loginResult.code != '000') return;
+      _navigateToMenu(loginResult.token, response.headers['set-cookie'] ?? "",
+          loginResult.menu);
+    }
+  }
+
+  Future<void> _checkBiometrics() async {
+    bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+      if (!mounted) return;
+      print(canCheckBiometrics);
+      setState(() {
+        _canCheckBiometrics = canCheckBiometrics;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiomterics;
+    try {
+      availableBiomterics = await auth.getAvailableBiometrics();
+      if (!mounted) return;
+      print(availableBiomterics);
+      setState(() {
+        _availableBiomterics = availableBiomterics;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticate(
+          localizedReason: 'Autenticati',
+          biometricOnly: true,
+          useErrorDialogs: true);
+      if (!mounted) return;
+      print(authenticated);
+      setState(() {
+        _authenticated = authenticated;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
 
   Future<http.Response> _login(String account, String password) async {
     final url = Uri.parse('https://infopol.poliziadistato.it/grab/new_panel/');
     Map<String, String> body = {'username': account, 'password': password};
     return await http.post(url, body: body);
+  }
+
+  ButtonStatus _statiBottone(String? a, String? b, bool? isLoading) {
+    if (isLoading != null && isLoading) return ButtonStatus.loading;
+    if (a != null && a.isNotEmpty && b != null && b.isNotEmpty)
+      return ButtonStatus.full;
+    return ButtonStatus.empty;
+  }
+
+  _navigateToMenu(String token, String cookie, List<Menu> menu) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => MenuList(token, cookie, menu)));
   }
 
   @override
@@ -28,62 +143,107 @@ class _LoginState extends State<Login> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(
-              initialValue: 'dstech',
-              enabled: false,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Account'),
-            ),
+            StreamBuilder<String>(
+                initialData: '',
+                stream: _accountStream.stream,
+                builder: (context, snapshot) {
+                  return TextFormField(
+                    controller: _accountController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Account'),
+                    onChanged: _accountStream.add,
+                  );
+                }),
             SizedBox(height: 20),
-            TextFormField(
-              initialValue: 'Polizia2021',
-              obscureText: true,
-              enabled: false,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Password'),
-            ),
+            StreamBuilder<String>(
+                initialData: '',
+                stream: _passwordStream.stream,
+                builder: (context, snapshot) {
+                  return TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Password'),
+                    onChanged: _passwordStream.add,
+                  );
+                }),
             Align(
               alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                child: Text('ACCEDI'),
-                onPressed: () async {
-                  BuildContext? dialogContext;
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      dialogContext = context;
-                      return Center(
-                        child: SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: CircularProgressIndicator(),
-                        ),
-                      );
-                    },
-                  );
-
-                  var response = await _login('dstech', 'Polizia2021');
-                  if (dialogContext != null) {
-                    Navigator.pop(dialogContext!);
-                  }
-                  var jsonStr = response.body;
-                  // print(response.headers['set-cookie']);
-                  if (response.statusCode == 200) {
-                    var loginResult = LoginResponse.fromJson(jsonStr);
-                    // print(jsonStr);
-                    // print(loginResult.token);
-                    if (loginResult.code == '000') {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MenuList(
-                                  loginResult.token,
-                                  response.headers['set-cookie'] ?? "",
-                                  loginResult.menu)));
-                    }
-                  }
-                },
-              ),
+              child: StreamBuilder<ButtonStatus>(
+                  initialData: ButtonStatus.empty,
+                  stream: CombineLatestStream.combine3(
+                      _accountStream.stream,
+                      _passwordStream.stream,
+                      _isLoadingStream.stream.startWith(false),
+                      (String? a, String? b, bool? c) =>
+                          _statiBottone(a, b, c)),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == ButtonStatus.loading)
+                      return CircularProgressIndicator();
+                    return ElevatedButton(
+                      child: Text('ACCEDI'),
+                      onPressed: snapshot.data == ButtonStatus.empty
+                          ? null
+                          : () async {
+                              _isLoadingStream.add(true);
+                              var response = await _login(
+                                  _accountController.text,
+                                  _passwordController.text);
+                              _isLoadingStream.add(false);
+                              var jsonStr = response.body;
+                              if (response.statusCode == 200) {
+                                var loginResult =
+                                    LoginResponse.fromJson(jsonStr);
+                                if (loginResult.code == '000') {
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return AlertDialog(
+                                          title: Text(
+                                              "Salvare i dati per il l'autenticazione biometrica?"),
+                                          actions: [
+                                            TextButton(
+                                                onPressed: () async {
+                                                  storage.write(
+                                                      key: 'account',
+                                                      value: _accountController
+                                                          .text);
+                                                  storage.write(
+                                                      key: 'password',
+                                                      value: _passwordController
+                                                          .text);
+                                                  Navigator.pop(context);
+                                                  _navigateToMenu(
+                                                      loginResult.token,
+                                                      response.headers[
+                                                              'set-cookie'] ??
+                                                          "",
+                                                      loginResult.menu);
+                                                },
+                                                child: Text('OK')),
+                                            TextButton(
+                                                onPressed: () {
+                                                  storage.delete(
+                                                      key: 'account');
+                                                  storage.delete(
+                                                      key: 'password');
+                                                  Navigator.pop(context);
+                                                  _navigateToMenu(
+                                                      loginResult.token,
+                                                      response.headers[
+                                                              'set-cookie'] ??
+                                                          "",
+                                                      loginResult.menu);
+                                                },
+                                                child: Text('Annulla'))
+                                          ],
+                                        );
+                                      });
+                                }
+                              }
+                            },
+                    );
+                  }),
             )
           ],
         ),
